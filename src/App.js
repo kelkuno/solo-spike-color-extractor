a1460c56160a0c4792995f91974715d18618d8ee
Kelsey Kuno <kelsey.kuno@gmail.com>
2022-09-05 17:32:25 -0500

    figured out how to rewrite it in functional component

--- src/App.js
+++ src/App.js
@@ -8,23 +8,43 @@ function App() {
   const [colors, setColors] = useState([]);
 
   const getColors = (colors) => {
-    setColors(colors);
-    // getColors ={colors => console.log(colors)}
+    setColors (colors);
+    
   }
+
+
+
+  console.log('colors before return', colors);
+  console.log('colors in brackets', {colors});
+  console.log('getColor function', getColors);
 
 
   return (
     <div className="App">
-      <header className="Kelsey">
+     
         <h1>React Library: react-color-extractor</h1>
+
         <ColorExtractor
-        getColors={colors => console.log('colors', colors)}
+        //passing in getColors function as a prop to ColorExtractor component 
+        getColors={getColors} 
          >
           <img src="https://media.istockphoto.com/photos/beautiful-sunset-over-the-tropical-sea-picture-id1172427455?b=1&k=20&m=1172427455&s=612x612&w=0&h=5e5ZY9KtcF86MolxsA1j_MgylIMjMJqXcqnOONVypO4=" className="sunset" alt="logo" />
+      
         </ColorExtractor>
+        <h3>This is what is returned from color extractor:</h3>
+        <p>{JSON.stringify(colors)}</p>
+        
         <button>Get colors!</button>  
-            
-      </header>
+
+        {/* <ul>
+          {colors.map(color => {
+            return
+            (<li>color</li>
+            );
+          })}
+        </ul> */}
+           
+   
     </div>
   );
 }
