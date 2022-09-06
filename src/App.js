5a504307e67aaa65003b4bf572a69b15b1e903e6
Kelsey Kuno <kelsey.kuno@gmail.com>
2022-09-06 10:51:43 -0500

    spike done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
+//✅import ColorExtractor component from library
 import { ColorExtractor } from 'react-color-extractor';
 import {useState} from 'react';
 
 
 function App() {
-  //set a local "colors" which is a local state w default state as empty array.
+  //✅set a local "colors" which is a local state w default state as empty array.
   const [colors, setColors] = useState([]);
 
-  //declared a getColors function that takes in props of "colors" and adds it too colors array.
+  //✅declared a getColors function that takes in props of "colors" and adds it too colors array.
   const getColors = (colors) => {
     setColors (colors);
   }
@@ -20,16 +21,17 @@ function App() {
     <div className="App">
      
         <h1>React Library: react-color-extractor</h1>
-        {/* wrapped the image in the ColorExtractor component (per instructions) */}
+        {/*✅ wrapped the image in the ColorExtractor component (per instructions) */}
         <ColorExtractor
-        //passing in getColors function as a prop to ColorExtractor component 
+        //✅passing in getColors function as a prop to ColorExtractor component 
         getColors={getColors} 
-        //maxColors is also a prop passed in, indicating the amount of colors in the photo to draw the palette color info from. 
+        //✅maxColors is also a prop passed in, tells the component how many colors to base the palette sample on. 
         maxColors={256}
          >
           <img src="https://media.istockphoto.com/photos/beautiful-sunset-over-the-tropical-sea-picture-id1172427455?b=1&k=20&m=1172427455&s=612x612&w=0&h=5e5ZY9KtcF86MolxsA1j_MgylIMjMJqXcqnOONVypO4=" className="sunset" alt="logo" />
       
         </ColorExtractor>
+        {/* ✅see DOM for visual */}
         <h3>This is what is returned from color extractor:</h3>
         <p>{JSON.stringify(colors)}</p>
          
